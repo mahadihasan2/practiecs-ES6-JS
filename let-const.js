00278e086a605f58576a23290f478dd22f92bc7c
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-21 16:32:34 +0600

    ES6-practiecs

---
+++ let-const.js
@@ -0,0 +1,19 @@
+//1.Let And Const diffarence
+const firstName = 'Mahadi'
+const lastName = "Hasan"
+const fullName = firstName + " " + lastName
+// console.log(fullName)
+
+/* lastName = firstName;
+firstName = 'Rimon'
+const fullName = lastName + firstName
+console.log(fullName) */
+
+let rollNumber = 505;
+let studentName = 'Rakib'
+let studentDetails = rollNumber + " " + studentName
+// console.log(studentDetails)
+rollNumber = 605
+studentName = 'Kadir'
+studentDetails = rollNumber + ' ' + studentName
+console.log(studentDetails)
