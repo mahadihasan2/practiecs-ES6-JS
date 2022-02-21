7fcb6b91b48dbef3fa8d7ccb9533f44a49457ce7
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-21 17:06:16 +0600

    template-string

---
+++ template-string.js
@@ -0,0 +1,13 @@
+// 2.a :Use Variable 
+const multiLine = `This is first Line
+this is second Line
+this is third Line
+this is fourth Line
+`
+console.log(multiLine)
+
+// 2.b: Use object property 
+const frirnds = ['imran', 'rakib', 'mahadi', 'sojib', 'jamal']
+const friendsCount = 10;
+const useTemplateString = `My friends List is: ${frirnds}.Her length is: ${frirnds.length}.Her total money is: ${(frirnds.length*friendsCount*100)}`
+console.log(useTemplateString)
